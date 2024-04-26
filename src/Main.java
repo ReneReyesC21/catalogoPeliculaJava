c5ef39bff424778cc057e1bce487e06420b75e0a
rene <canedarene@gmail.com>
2024-04-25 18:12:46 -0600

    Empezando el catalago

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenid@ a Tu SalaTV");
+        System.out.println("Peliculas: Matrix");
+
+
+        int fechaDeLanzamiento = 1999;
+    }
+}
