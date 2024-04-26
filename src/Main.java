9ca2cb0ec76c80b59a922be896affa31d8445e6f
rene <canedarene@gmail.com>
2024-04-25 19:02:02 -0600

    agregado de sinopsis y de clasificacion

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,29 @@ public class Main {
 
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnPlan = true;
-        double notaPelicula = 8.2;
+        //double notaPelicula = 8.2;
+
         double media = (8.2 + 6.0 + 9.0)/3;
         System.out.println(media);
+
+        String sinopsis = """
+                En un mundo aparentemente normal pero plagado de misterios,
+                Thomas Anderson, un programador de software por el día y hacker por la noche, 
+                se encuentra atrapado en una realidad que no puede comprender. Cuando un enigmático hombre 
+                llamado Morpheus lo contacta, Anderson descubre la verdad impactante: su vida y la de la humanidad 
+                entera son una ilusión creada por una inteligencia artificial conocida como la Matrix.
+                Ahora, junto con un grupo de rebeldes, Anderson, 
+                quien ahora se hace llamar Neo, se embarca en una odisea para liberar a la humanidad del 
+                control opresivo de las máquinas. Lleno de acción trepidante, efectos visuales innovadores 
+                y profundas reflexiones filosóficas, 'Matrix' desafía los límites de la realidad y explora 
+                las complejidades del libre albedrío, la identidad y la lucha por la verdad en un mundo donde nada es lo que parece. 
+                Prepárate para adentrarte en un universo donde la realidad se desvanece y las preguntas 
+                sobre la existencia misma son inevitables.
+                Esta pelicula fue lanzada en :
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media/2);
+        System.out.println(clasificacion);
     }
 }
